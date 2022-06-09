39802bc6e02c0634d9f6799c74c5d0aa46391505
simo2898 <70075823+simo2898@users.noreply.github.com>
2022-06-09 10:54:08 +0200

    Add home

---
+++ space_invaders/cognome_nome_nickname_a_scelta/src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import router from "./router";
+export default {
+  name: "App",
+  components: { RouterLink, RouterView },
+};
+</script>
+
+<template>
+  <div class="wrapper">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+h1 {
+  color: white;
+}
+
+.wrapper {
+  width: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #1d1b1d;
+}
+
+#app {
+}
+
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.logo {
+  margin-top: 150px;
+  margin-bottom: 70px;
+  width: 50%;
+}
+</style>
---
+++ space_invaders/cognome_nome_nickname_a_scelta/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <header tabindex="1" @keyup.enter="prova">
+    <img alt="Space logo" class="logo" src="@/assets/space-invaders-logo.png" />
+    <h1>Press enter to start the game</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+
+  methods: {
+    prova() {
+      console.log("Enter");
+      this.$router.push({ name: "game" });
+      this.home = false;
+      console.log(this.home);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
