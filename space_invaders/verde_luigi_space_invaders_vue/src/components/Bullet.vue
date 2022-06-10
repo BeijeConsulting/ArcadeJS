0fd997de0117e70f8dc11e0535711755bfe058ae
GinoGreen <luigiverde97@gmail.com>
2022-06-10 10:03:48 +0200

    bullet

---
+++ space_invaders/verde_luigi_space_invaders_vue/src/components/Bullet.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+   name: "Bullet"
+}
+</script>
+
+<style>
+   .bullet {
+      width: 4px;
+      height: 10px;
+      background-color: #cf4072;
+      position: absolute;
+   }
+
+</style>
--- space_invaders/verde_luigi_space_invaders_vue/src/views/GameView.vue
+++ space_invaders/verde_luigi_space_invaders_vue/src/views/GameView.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="game">
+   <div id="game" class="game" ref="game">
       <!-- INVADER TYPE 1 -->
       <div v-for="(row, index) in invaderType1_Rows" :key="index + 'rows 1'" class="my_space">
          <Invader v-for="(invader, index) in invaderPerRow"
@@ -24,21 +24,26 @@
          />
       </div>
 
-      <div class="player">
+      <div id="player" ref="player" class="player">
          <div class="head_player"></div>
          <div class="body_player"></div>
       </div>
+      <Bullet />
    </div>
 </template>
 
 <script>
+import { createApp } from 'vue'
+import gsap from "gsap"
 import Invader from "@/components/Invader.vue"
+import Bullet from "@/components/Bullet.vue"
 
 export default {
    name: "GameView",
 
    components: {
-      Invader
+      Invader,
+      Bullet
    },
 
    data() {
@@ -49,8 +54,50 @@ export default {
          invaderType2_Rows: 2,
          invaderType3_Rows: 2,
 
+         countBullets: 0
 
       }
+   },
+
+   methods: {
+      shoot() {
+         this.createBullet();
+         const xPoint = this.$refs.player.offsetLeft - 2.5 ;
+         
+         gsap.fromTo("#bullet_" + (this.countBullets - 1), 
+            {
+               left: xPoint,
+               bottom: 55
+            },
+            {
+               bottom: "100%",
+               duration: 1.6,
+               ease: "linear"
+            }
+         );
+      },
+      createBullet() {
+         const game = document.getElementById("game");
+         const bullet = document.createElement("div");
+         bullet.id = "bullet_" + this.countBullets++;
+         bullet.classList.add("bullet");
+         game.appendChild(bullet);   
+      }
+   },
+
+   mounted() {
+
+      //SHOOT EVERY .4s
+      let timeout = false;
+      window.addEventListener("keypress", (event) => {
+         if (!timeout && event.key == " ") {
+            this.shoot();
+            timeout = true;
+            setTimeout(function() {
+               timeout = false;  
+            }, 400);
+         }
+      });
    }
 }
 </script>
